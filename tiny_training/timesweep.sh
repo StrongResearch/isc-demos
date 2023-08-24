d54d915215d8701462454d37822223e4e7a6bbf4
strongchris <chris@strongcompute.com>
2023-08-24 04:08:29 +0000

    remove some unsupported code

--- tiny_training/timesweep.sh
+++
@@ -1,9 +0,0 @@
-export MASTER_ADDR=10.10.10.68
-export MASTER_PORT=12356
-export LOCAL_ADDR=`hostname -i | awk '{print $1}'`
-# time torchrun --nnodes=1 --nproc_per_node=1 --rdzv_id=100 --rdzv_backend=c10d --rdzv_endpoint=$MASTER_ADDR:$MASTER_PORT train.py $@
-# time torchrun --nnodes=1 --nproc_per_node=2 --rdzv_id=100 --rdzv_backend=c10d --rdzv_endpoint=$MASTER_ADDR:$MASTER_PORT train.py $@
-# time torchrun --nnodes=1 --nproc_per_node=3 --rdzv_id=100 --rdzv_backend=c10d --rdzv_endpoint=$MASTER_ADDR:$MASTER_PORT train.py $@
-# time torchrun --nnodes=1 --nproc_per_node=6 --rdzv_id=100 --rdzv_backend=c10d --rdzv_endpoint=$MASTER_ADDR:$MASTER_PORT train.py $@
-# time torchrun --nnodes=2 --nproc_per_node=1 --rdzv_id=100 --rdzv_backend=c10d --rdzv_endpoint=$MASTER_ADDR:$MASTER_PORT train.py $@
-time torchrun --nnodes=2 --nproc_per_node=1 --rdzv_id=100 --rdzv_backend=c10d --rdzv_endpoint=$MASTER_ADDR:$MASTER_PORT --local-addr=$LOCAL_ADDR train.py $@
