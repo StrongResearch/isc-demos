a68d8370f1bb31e8b4978ec5609cf9f8528c5adb
strongchris <chris@strongcompute.com>
2023-08-31 06:15:39 +0000

    fix errors from first testing roung

---
+++ tv-segmentation/prep.py
@@ -0,0 +1,5 @@
+import torchvision
+
+weights = torchvision.models.get_weight('MobileNet_V3_Large_Weights.IMAGENET1K_V1')
+weights = torchvision.models.get_weight('ResNet101_Weights.IMAGENET1K_V1')
+
