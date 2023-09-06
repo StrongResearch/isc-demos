0a9c0c6b61d4e0d28850abb028a89c9b11fa9d2c
StrongCalvin <calvin@strongcompute.com>
2023-09-06 23:41:26 +0000

    typo

--- lightning-examples/small_language_model.py
+++ lightning-examples/small_language_model.py
@@ -10,33 +10,8 @@ import lightning as L
 from lightning.pytorch.demos import Transformer, WikiText2
 import torch.distributed as dist
 from lightning.pytorch.callbacks import ModelCheckpoint
-from cycling_utils import InterruptableDistributedSampler, atomic_torch_save
+from cycling_utils import InterruptableDistributedSampler, atomic_torch_save, EpochHandler
 from lightning.pytorch.strategies import DDPStrategy
-
-
-class EpochHandler(L.Callback):
-
-    def __init__(self, sampler, checkpoint_dir, save_freq):
-        super().__init__()
-        self.sampler = sampler
-        self.checkpoint_dir = checkpoint_dir
-        self.save_freq = save_freq
-
-    def on_train_epoch_end(self, trainer, pl_module):
-        self.sampler._reset_progress()
-
-    def on_train_batch_start(self, trainer, pl_module, batch, batch_idx):
-        self.sampler.advance(len(batch))
-
-    def on_train_batch_end(self, trainer, pl_module, outputs, batch, batch_idx):
-
-        self.sampler.advance(len(batch))
-        
-        if batch_idx % self.save_freq == 0 and dist.get_rank() == 0:
-
-            atomic_torch_save({
-                "sampler_state_dict": self.sampler.state_dict(),
-            }, f"{self.checkpoint_dir}/sampler_last.pt")
 
 
 class LanguageModel(L.LightningModule):
@@ -127,8 +102,10 @@ def main():
 
         trainer.fit(model=model, train_dataloaders=train_dataloader, val_dataloaders=val_dataloader, ckpt_path=checkpoint_latest / 'last.ckpt')
     else:
-        trainer.fit(model=model, train_dataloadesr=train_dataloader, val_dataloaders=val_dataloader)
+        trainer.fit(model=model, train_dataloaders=train_dataloader, val_dataloaders=val_dataloader)
     trainer.test(model, test_dataloader)
+
+    raise Exception
 
 
 if __name__ == "__main__":
