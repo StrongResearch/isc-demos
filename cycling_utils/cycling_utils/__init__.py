6be99bd48d095819886b387d1aa2a2a49d12d29d
Michael S <116326048+StrongMichaelS@users.noreply.github.com>
2023-09-22 14:58:32 +1000

    Update __init__.py

--- cycling_utils/cycling_utils/__init__.py
+++ cycling_utils/cycling_utils/__init__.py
@@ -1,5 +1,12 @@
 from .saving import atomic_torch_save
 from .sampler import InterruptableDistributedSampler
-from .lightning_utils import EpochHandler
 
-__all__ = ["InterruptableDistributedSampler", "atomic_torch_save", "EpochHandler"]
+__all__ = ["InterruptableDistributedSampler", "atomic_torch_save"]
+
+try:
+    import lightning as L
+    HAS_LIGHTNING = True
+    from .lightning_utils import EpochHandler
+    __all__.append("EpochHandler")
+except ImportError:
+    HAS_LIGHTNING = False
