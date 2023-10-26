b8d35df9a048d54b04d5fed2612e84ed73b56ed8
adaadam4qalumfvjdstjpx7zyvlebh2u <adam@strongcompute.com>
2023-10-26 15:26:02 +1100

    Removed cycling_utils from isc-demos

--- cycling_utils/cycling_utils/__init__.py
+++
@@ -1,5 +0,0 @@
-from .saving import atomic_torch_save
-from .sampler import InterruptableDistributedSampler
-from .lightning_utils import EpochHandler
-
-__all__ = ["InterruptableDistributedSampler", "atomic_torch_save", "EpochHandler"]
