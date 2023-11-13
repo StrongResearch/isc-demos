34e73fe91c4a689b6d0e4d7568e2254ce9754310
StrongLachlanM <lachlan.murphy@strongcompute.com>
2023-11-13 07:07:48 +0000

    update prep

--- stanford/CompRx/prep.py
+++ stanford/CompRx/prep.py
@@ -1,6 +1,8 @@
 import torchvision.models as models
 from torchvision.models import VGG16_Weights
 from torch_fidelity import FeatureExtractorInceptionV3
+from clip import load
 
 vgg16 = models.vgg16(weights=VGG16_Weights.IMAGENET1K_V1)
 FeatureExtractorInceptionV3("a", ["logits"])
+load("ViT-B/32", "cpu")
