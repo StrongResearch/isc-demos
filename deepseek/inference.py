be36219eda013d9ada48ea14452350d08a324157
zazer1 <zac@strongcompute.com>
2025-02-22 02:15:23 +0000

    feat(inference): add helper comments for clarity!

--- deepseek/inference.py
+++ deepseek/inference.py
@@ -1,3 +1,4 @@
+### INFO: This is a helper script to allow participants to confirm their model is working!
 import torch
 import torch.distributed.checkpoint as dcp
 from transformers import AutoTokenizer, AutoModelForCausalLM
@@ -7,9 +8,24 @@ from fsdp_utils import AppState
 
 adapter_name = "ExampleLora"
 
-tokenizer = AutoTokenizer.from_pretrained("/data")
+# INFO: This is a helper to map model names to StrongCompute Dataset ID's which store their weights!
+model_weight_ids = {
+    "DeepSeek-R1-Distill-Llama-70B": "e4b2dc79-79af-4a80-be71-c509469449b4",
+    "DeepSeek-R1-Distill-Llama-8B": "38b32289-7d34-4c72-9546-9d480f676840",
+    "DeepSeek-R1-Distill-Qwen-1.5B": "6c796efa-7063-4a74-99b8-aab1c728ad98",
+    "DeepSeek-R1-Distill-Qwen-14B": "39387beb-9824-4629-b19b-8f7b8f127150",
+    "DeepSeek-R1-Distill-Qwen-32B": "84c2b2cb-95b4-4ce6-a2d4-6f210afad36b",
+    "DeepSeek-R1-Distill-Qwen-7B": "a792646c-39f5-4971-a169-425324fec87b",
+}
+
+# TODO: set this to the model you chose from the dropdown at container startup!
+MODEL_NAME_SETME = "DeepSeek-R1-Distill-Qwen-1.5B"
+mounted_dataset_path = f"/data/{model_weight_ids[MODEL_NAME_SETME]}"
+
+# INFO: Loads the model WEIGHTS (assuming you've mounted it to your container!)
+tokenizer = AutoTokenizer.from_pretrained(mounted_dataset_path)
 model = AutoModelForCausalLM.from_pretrained(
-    "/data", 
+    mounted_dataset_path, 
     use_cache=False, 
     torch_dtype=torch.bfloat16
 )
